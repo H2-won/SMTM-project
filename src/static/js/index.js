0a9f4534ee11b3bbc308f7abea077bf9cb46b532
LIMHUIWON <59960678+LIMHUIWON@users.noreply.github.com>
2021-01-14 19:34:43 +0900

    모바일 touch event - parallax 적용 (21.01.14)

--- src/static/js/index.js
+++ src/static/js/index.js
@@ -10,6 +10,18 @@ function parallax(e) {
     });
 }
 
+document.addEventListener('touchmove', mobileParallax);
+function mobileParallax(e) {
+    this.querySelectorAll('.layer').forEach(layer => {
+        const speed = layer.getAttribute('mobile-data-speed');
+
+        const x = (window.innerWidth - e.changedTouches[0].pageX * speed) / 100;
+        const y = (window.innerHeight - e.changedTouches[0].pageY * speed) / 100;
+        
+        layer.style.transform = `translateX(${x}px) translateY(${y}px)`;
+    });
+}
+
 const startBtn = document.querySelector('.startBtn');
 startBtn.addEventListener('click', () =>{
     window.location.href="/main";
