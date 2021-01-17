0fee0ea3731eba3146f49b5f6969ea5190663d9a
LIMHUIWON <59960678+LIMHUIWON@users.noreply.github.com>
2021-01-18 01:04:33 +0900

    에러 핸들러 수정 (21.01.18)

--- src/static/js/main.js
+++ src/static/js/main.js
@@ -306,6 +306,5 @@ function getRapperDescriptionAPI(rapperName) {
 function errorHandler(error) {
     if (!error || !(error.res)) {
         console.log(error);
-        alert('에러가 발생하였습니다. 다시 시도해주세요.');
     }
 }
