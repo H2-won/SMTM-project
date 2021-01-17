956909ee29eb9273094d0195f6d08c4ee739ba11
woosin0113 <74348790+woosin0113@users.noreply.github.com>
2021-01-18 01:14:33 +0900

    DB connection(21.01.18)

--- src/app.py
+++ src/app.py
@@ -1,15 +1,31 @@
 from flask import *
+import json
+from db import selectAll, selectAlls
 
 app = Flask(__name__, instance_relative_config=True)
 
+
 @app.route('/')
 def index():
-       return render_template('index.html')
+    return render_template('index.html')
+
 
 @app.route('/main')
 def main():
-       return render_template('main.html')
+    return render_template('main.html')
 
-### 실행
+
+@app.route('/rapper', methods=['POST'])
+def rapper():
+    t = selectAlls()
+    print(t)
+    name = request.get_json()
+    for n in t:
+        if (n['name'] == name['name']):
+            return jsonify(n['description'])
+    return 'hi'
+
+
+# 실행
 if __name__ == '__main__':
-	app.run(host='0.0.0.0', debug=True, port='5000')
+    app.run(host='0.0.0.0', debug=True, port='5000')
